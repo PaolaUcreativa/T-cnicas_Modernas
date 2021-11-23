b7dfeb939e11edf1daeb6e30103c18c690268f7d
PaolaUcreativa <maria.rojas.valverde@ucreativa.com>
2021-11-22 22:25:48 -0600

    Tarea01

---
+++ test/pageobjects/Inputs.page.js
@@ -0,0 +1,28 @@
+import Page from "./page";
+
+class InputsPage extends Page{
+    /**
+     * define selectors using getter methods
+     */
+     get getTitle(){
+        return $('h3');
+     } 
+
+     get getSubtitle(){
+        return $('p');
+     } 
+
+    get getInput(){
+        return $('input[type=number]');
+    }
+
+    setValueInput(){
+        return this.getInput.setValue(10);
+    }
+
+    currentInput(){
+        return $('#content > div > div > div > input[type=number]');
+    }
+}
+
+export default new InputsPage();
---
+++ test/specs/inputsapptest.spec.js
@@ -0,0 +1,33 @@
+import InputsPage from '../pageobjects/inputs.page';
+
+describe('My Inputs App', () => {
+    beforeAll(async function(){
+        console.log('I get executed before all its.');
+    });
+    beforeEach(async function(){
+        console.log('I get executed before every it');
+        await InputsPage.open('inputs');
+    });
+    afterAll(async function(){
+        console.log('I get executed after all its.');
+    });
+    afterEach(async function(){
+        console.log('I get executed after every it');
+        await InputsPage.open('inputs');
+    });
+
+
+    it('Should open the inputs app', async () => {
+        console.log('first test');
+        await expect(InputsPage.getTitle).toHaveTextContaining('Inputs');
+    });
+    it('Should have subtitle', async () => {
+        console.log('second test');
+        await expect(InputsPage.getSubtitle).toHaveTextContaining('Number');
+    });
+    it('Set number', async () => {
+        console.log('third test');
+        await InputsPage.setValueInput();
+         expect(InputsPage.currentInput()).toHaveTextContaining(10);
+    });
+}); 
